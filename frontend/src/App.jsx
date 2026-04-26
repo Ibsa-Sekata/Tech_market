01dbe61352cc58a8d711156979ed049dc7b41c4f
ibsa sekata <ibsaaseek@gmail.com>
2026-04-26 19:26:48 -0300

    feat: implement main application structure with routing and layout

---
+++ frontend/src/App.jsx
@@ -0,0 +1,71 @@
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import ProtectedRoute from "./components/ProtectedRoute";
+import AdminDashboardPage from "./pages/AdminDashboardPage";
+import CartPage from "./pages/CartPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import OrderDetailPage from "./pages/OrderDetailPage";
+import OrderHistoryPage from "./pages/OrderHistoryPage";
+import ProductDetailPage from "./pages/ProductDetailPage";
+import ProfilePage from "./pages/ProfilePage";
+import RegisterPage from "./pages/RegisterPage";
+import ShopPage from "./pages/ShopPage";
+
+export default function App() {
+  return (
+    <Layout>
+      <Routes>
+        <Route element={<HomePage />} path="/" />
+        <Route element={<ShopPage />} path="/shop" />
+        <Route element={<ProductDetailPage />} path="/products/:id" />
+        <Route element={<CartPage />} path="/cart" />
+        <Route element={<LoginPage />} path="/login" />
+        <Route element={<RegisterPage />} path="/register" />
+        <Route
+          element={
+            <ProtectedRoute>
+              <CheckoutPage />
+            </ProtectedRoute>
+          }
+          path="/checkout"
+        />
+        <Route
+          element={
+            <ProtectedRoute>
+              <ProfilePage />
+            </ProtectedRoute>
+          }
+          path="/profile"
+        />
+        <Route
+          element={
+            <ProtectedRoute>
+              <OrderHistoryPage />
+            </ProtectedRoute>
+          }
+          path="/orders"
+        />
+        <Route
+          element={
+            <ProtectedRoute>
+              <OrderDetailPage />
+            </ProtectedRoute>
+          }
+          path="/orders/:id"
+        />
+        <Route
+          element={
+            <ProtectedRoute adminOnly>
+              <AdminDashboardPage />
+            </ProtectedRoute>
+          }
+          path="/admin"
+        />
+        <Route element={<NotFoundPage />} path="*" />
+      </Routes>
+    </Layout>
+  );
+}
